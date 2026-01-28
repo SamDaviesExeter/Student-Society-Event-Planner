5af65613747dba2f3379771380391228a612736f
caaaleb106 <ch1271@exeter.ac.uk>
2026-01-28 17:55:48 +0000

    Update file_read.py

--- source/file_read.py
+++ source/file_read.py
@@ -1,12 +1,19 @@
 
 
-def read_file(file):
-    try:
-        with open(file, "r") as f:
-            file = f.readlines()
-            
-    finally:
-        f.close()
-    return file
+def read_file(file:str)-> list[str]:
+    with open(file, "r") as f:
+        file_array = f.read().splitlines()
 
-print(read_file("assets/input_small.txt"))
+    return file_array
+
+def split_lines(array:list):
+    number_act = array[0]
+    constraints = array[1]
+    activites = array[2:]
+    return number_act, constraints, activites
+
+def split_constraints(constraints:str)-> list:
+    return constraints.split()
+
+
+print(split_constraints("10 200"))
