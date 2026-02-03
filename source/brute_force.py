e42c61dfb18a39c58fa19ff8e4a356e80ceee1c3
caaaleb106 <ch1271@exeter.ac.uk>
2026-02-03 18:15:46 +0000

    added constraints

--- source/brute_force.py
+++ source/brute_force.py
@@ -1,1 +1,12 @@
+import file_opperations
+
+#FOCUS ON BUDGET
+constraint = "budget"
+constraints_position = {"time": 0,"budget": 1}
+
+def get_constraint(): # itterate threw a for loop for both values later
+    return constraints_position[constraint]
+
+constraint_value =file_opperations.get_targeted_constraint("assets/input_small.txt",get_constraint())
+
 
--- source/file_opperations.py
+++ source/file_opperations.py
@@ -15,7 +15,16 @@ def split_lines(array:list):
 def split_constraints(constraints:str)-> list[str]:
     return constraints.split() #time index 0 , budget index 1
 
+def get_constranint(constraint:int,array:list[str])-> str:
+    return array[constraint]
 
+def get_targeted_constraint(file_path,choice):
+    file = read_file(file_path)
+    number_act, constraints, activites = split_lines(file)
+    constraints_split = split_constraints(constraints)
+    con = get_constranint(choice,constraints_split)
+    return con
 
 if __name__ == "__main__":
     ...
+    
